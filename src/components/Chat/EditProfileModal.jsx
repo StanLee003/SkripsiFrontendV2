6eba77046dc06e46ecae9756148eee058083c974
StanLee003 <86097005+StanLee003@users.noreply.github.com>
2025-07-27 22:38:15 +0700

    Update EditProfileModal.jsx

--- src/components/Chat/EditProfileModal.jsx
+++ src/components/Chat/EditProfileModal.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { X, Camera } from 'lucide-react';
 
 import InputField from '../Auth/InputField';
-import languages from '../../utils/languages';
-import systemLanguages from '../../utils/languages';
+import { languages, systemLanguages } from '../../utils/languages';
 import { BACKEND_URL } from '../../backend';
 import { axiosWithAuth } from '../../utils/axiosWithAuth';
 
