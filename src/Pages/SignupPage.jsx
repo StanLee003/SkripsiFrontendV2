a8bc4ba7ff3cb2d35f900a917984132e8be16bc9
StanLee003 <86097005+StanLee003@users.noreply.github.com>
2025-08-05 10:13:44 +0700

    Delete src/Pages directory

--- src/Pages/SignupPage.jsx
+++
@@ -1,5 +0,0 @@
-// src/pages/SignupPage.jsx
-import { sendVerificationEmail } from "../utils/sendEmailVerification";
-
-await sendVerificationEmail(userLanguage || 'en');
-alert('Link verifikasi sudah dikirim ke email Anda.');
